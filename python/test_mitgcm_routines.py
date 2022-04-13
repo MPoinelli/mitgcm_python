5d83d117a1b77ecfa3bcac722236a994735a37f6
MPoinelli <m.poinelli@tudelft.nl>
2022-04-12 18:15:18 -0700

    Add test template
    Add template test script

--- python/test_mitgcm_routines.py
+++ python/test_mitgcm_routines.py
@@ -7,5 +7,18 @@ Created on Tue Apr 12 17:58:58 2022
 """
 
 
-def createDFfrommultipleSTD():
+def test_GenMonitorDiagnostics():
+    #assert(GenMonitorDiagnostics)==?
+    pass
+
+def test_diagnosticsList():
+    #assert(diagnosticsList)==?
+    pass
+
+def test_createDFfrommultipleSTD():
+    #assert(createDFfrommultipleSTD)==?
+    pass
+
+def test_MonitorSTDOUT():
+    #assert(MonitorSTDOUT)==?
     pass
