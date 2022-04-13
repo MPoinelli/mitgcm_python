3b1616589b32485c110608192c4ebc48578126a4
MPoinelli <m.poinelli@tudelft.nl>
2022-04-12 17:55:52 -0700

    More on revert to single repo

--- python/mitgcm_routines.py
+++ python/mitgcm_routines.py
@@ -4,6 +4,56 @@ Main module to work with STDOUT files
 
 import pandas as pd
 from matplotlib import pyplot as plt
+
+def GenMonitorDiagnostics():
+    """
+    Write MonitorDiagnostics.txt based on a specific STDOUT file
+
+    Returns
+    -------
+    None.
+
+    """
+
+    PATH = '/nobackupp11/mpoinell/larsen/run_larsen00e/output/'
+
+    with open(PATH + 'STDOUT.437760', 'r') as std_file:
+        column = []
+
+        for line_number, line in enumerate(std_file.readlines()):
+
+            # generate dataframe columns
+            if (line_number > 4045 and line_number < 4277) and \
+                    (line[25:27] != '==' and line[25:27] != 'd ' and line[25:27] != 'gi'):
+                column.append(line[25:45].rstrip())
+
+        with open('MonitorDiagnostics.txt', 'w') as std_var:
+            for variable in column:
+                std_var.write(variable + '\n')
+
+
+def diagnosticsList():
+    """
+    Routine to read MonitorDiagnostics.txt
+
+    Returns
+    -------
+    column : list of diagnostics
+    """
+
+    # set the path to MonitorDiagnostics.txt, created with genmonitorDiagnostics
+    MonitorDiagnostics = 'MonitorDiagnostics.txt'
+
+    # Initialize empy list
+    diagnostics_list = list()
+
+    with open(MonitorDiagnostics, 'r') as std_var:
+        # read line and append it to
+        for line in std_var.readlines():
+            diagnostics_list.append(line.rstrip())
+
+    return diagnostics_list
+
     
 def createDFfrommultipleSTD(*files):
     """
--- python/verification.py
+++ python/verification.py
@@ -9,6 +9,6 @@ Created on Fri Apr  1 17:20:49 2022
 import mitgcm_routines
 
 
-file = '/nobackupp11/mpoinell/larsen/run_larsen03c/STDOUT.0000'
-df_STDOUT = mitgcm_routines.createDFfrommultipleSTD(file)
-#mitgcm_routines.MonitorSTDOUT(file)
+file = 'tests/STDOUT.0000'
+#df_STDOUT = mitgcm_routines.createDFfrommultipleSTD(file)
+mitgcm_routines.MonitorSTDOUT(file)
